c3a89b35be3aacc7c09d2dd3369704ac038b6409
Kachi1001 <felipekaim234@gmail.com>
2024-10-18 16:15:55 -0300

    corrigindo media

--- Site_django/settings.py
+++ Site_django/settings.py
@@ -109,13 +109,13 @@ USE_TZ = True
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
-MEDIA_URL = 'api/media/'
+MEDIA_URL = 'media/'
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'api/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
