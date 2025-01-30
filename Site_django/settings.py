763049c54f57bb7f8abf6c138030dcc7b7177ef2
Kachi1001 <felipekaim234@gmail.com>
2025-01-30 17:48:08 -0300

    teste

--- Site_django/settings.py
+++ Site_django/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Site_django.logger.LoggingMiddleware' 
+    'Home.logger.LoggingMiddleware' 
 ]
 
 CORS_ALLOWED_ORIGINS = [
