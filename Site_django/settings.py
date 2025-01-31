440f53c70e18b25218250b95c1567458d50592ff
Kachi1001 <felipekaim234@gmail.com>
2025-01-31 15:04:43 -0300

    moloide

--- Site_django/settings.py
+++ Site_django/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'Home.logger.LoggingMiddleware' 
+    'Site_django.logger.LoggingMiddleware' 
 ]
 
 CORS_ALLOWED_ORIGINS = [
